6287ebc0ff822cb89ea39783323beb83e98ff286
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2024-11-22 21:23:15 +0700

    menambah fungsi insert dan delete

--- pertemuan10/functions.php
+++ pertemuan10/functions.php
@@ -3,9 +3,9 @@
 // Koneksi DB
 $conn = mysqli_connect("localhost", "root", "", "phpdasar");
 
-function query($querry) {
+function query($query) {
     global $conn;
-    $result = mysqli_query($conn, $querry);
+    $result = mysqli_query($conn, $query);
     $rows = [];
     while ( $row = mysqli_fetch_assoc($result) ) {
         $rows[] = $row;
@@ -13,4 +13,28 @@ function query($querry) {
     return $rows;
 }
 
+function tambah($data) {
+    global$conn;
+    
+    // Ambil data dari setipa elemen form
+    $nrp = htmlspecialchars($data["nrp"]);
+    $nama = htmlspecialchars($data["nama"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+
+    // Query insert data
+    //VALUES null untuk mengosongkan inputan pada id karena AI
+    $query = "INSERT INTO mahasiswa VALUES(null,'$nrp','$nama','$email','$jurusan','$gambar')"; 
+
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+function hapus($id) {
+    global $conn;
+    mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id");
+    return mysqli_affected_rows($conn);
+}
 ?>
